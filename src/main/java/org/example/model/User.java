b2e8ccb76cd8f60021f84b5845dfba5e968ffcee
Negar Baharmand <negarbaharmand@outlook.com>
2023-10-02 09:48:16 +0200

    Complete User class

--- src/main/java/org/example/model/User.java
+++ src/main/java/org/example/model/User.java
@@ -1,2 +1,53 @@
-package org.example.model;public class User {
+package org.example.model;
+
+import java.security.SecureRandom;
+import java.util.Random;
+
+public class User {
+    private final String username;
+    private String password;
+
+    // This is used for creating user
+    public User(String username) {
+        this.username = username;
+        newPassword();
+    }
+
+    // This constructor is used for authentication
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String userInfo() {
+        return "User" + username + " has been created successfully. " + "PWD: " + password;
+    }
+
+    public void newPassword() {
+        this.password = generateRandomPassword();
+    }
+
+
+    private String generateRandomPassword() {
+        String allowedCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-_=+";
+        int passwordLength = 10;
+        StringBuilder stringBuilder = new StringBuilder();
+        Random random = new SecureRandom();
+        for (int i = 0; i < passwordLength; i++) {
+            int randomIndex = random.nextInt(allowedCharacters.length());
+            char randomChar = allowedCharacters.charAt(randomIndex);
+            stringBuilder.append(randomChar);
+        }
+        return stringBuilder.toString();
+
+    }
+
 }
